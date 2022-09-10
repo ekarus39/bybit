ab47611742ca37d3304429eaf249d848af17c824
ekarus39 <ekarus39@gmail.com>
2022-09-10 11:55:50 +0900

    make it better

--- gettingstarted/wsgi.py
+++ gettingstarted/wsgi.py
@@ -1,9 +1,8 @@
+import string
+
 from flask import Flask, request, json
 import ccxt
 import pprint
-
-api_key = "R19apmpdek5DXoCEshvxg3yEVLCxoF5SfGM42QLntHFuLVnz3qFxw5yrKtqeDwX0"
-secret = "SxCJoYL2Xi2LHsuCZRzBfWA8Go1uS3JgeZ4eFSkbN1EvOoMggTOUisTclqfMVRbh"
 
 app = Flask(__name__)
 
@@ -14,12 +13,10 @@ def index():
 @app.route('/webhook', methods = ['POST'])
 def webhook():
 
-    symbol1 = "BTCUSDT"
-    symbol2 = "BTC/USDT"
-
-    data = json.loads(request.data)
-    # pprint.pprint(data['order'] )
-    # return 'Hello, webhook!'
+    with open("../binance-apiKey.txt") as f:
+        lines = f.readlines()
+        api_key = lines[0].strip()
+        secret = lines[1].strip()
 
     # binance 객체 생성
     binance = ccxt.binance(config={
@@ -31,111 +28,119 @@ def webhook():
         }
     })
 
-    # //USDS-M 선물 잔고를 조회
+    # 트레이딩뷰에서 보내온 알림해석
+    data = json.loads(request.data)
+    orderType = data['order']
+    symbol = data['ticker'][0:len(data['ticker']) - 4] + "/" + data['ticker'][-4:]
+
+    # 바이낸스에 USDS-M 선물 잔고조회
     balance = binance.fetch_balance(params={"type": "future"})
-    # balance..set_position_mode(hedged=True, symbol=symbol2)
     positions = balance['info']['positions']
-    positionAmt = 0
-    # leverage = 0
+    positionAmt = 0.0
+    leverage = 0
+
     for position in positions:
-        if position["symbol"] == symbol1:
-            # pprint.pprint(position)
+        if position["symbol"] == data['ticker']:
             positionAmt = float(position['positionAmt'])
+            # 현재 설정되어있는 레버라지 취득
             leverage = float(position['leverage'])
 
-    # 현금보유액
+    # 구입가능현금보유액
     cash = float(balance['USDT']['free'])
     # 현재가격조회
-    price1 = binance.fetch_ticker(symbol2)
-    current_price = float(price1['last'])
-
+    current_price = float(binance.fetch_ticker(symbol)['last'])
     # 주문가능수량
     qty = (cash/current_price) * (leverage - 0.5)
+    # 롱포자션 손절퍼센트 설정
+    longStopPrice = 0.98
+    # 숏포자션 손절퍼센트 설정
+    shortStopPrice = 1.02
 
-    # //트레이딩뷰 해석
-    orderType = data['order']
-
-    # 보유포지션이 없는경우
+    # 보유포지션이 없는경우 신규주문
     if float(positionAmt) == 0:
         if orderType == "buy":
             # 매수/롱 포지션 진입
             binance.create_order(
-                symbol=symbol2,
+                symbol=symbol,
                 type="MARKET",
                 side="buy",
                 amount=qty
             )
             # stop loss
             binance.create_order(
-                symbol=symbol2,
+                symbol=symbol,
                 type="STOP_MARKET",
                 side="sell",
                 amount=qty,
-                params={'stopPrice': current_price*0.98}
+                params={'stopPrice': current_price * longStopPrice}
             )
         if orderType == "sell":
             # 매도/숏 포지션 진입
             binance.create_order(
-                symbol=symbol2,
+                symbol=symbol,
                 type="MARKET",
                 side="sell",
                 amount=qty
             )
             # stop loss
             binance.create_order(
-                symbol=symbol2,
+                symbol=symbol,
                 type="STOP_MARKET",
                 side="buy",
                 amount=qty,
-                params={'stopPrice': current_price*1.02}
+                params={'stopPrice': current_price * shortStopPrice}
             )
     # 포지션 보유중인 경우
     else:
         if orderType == "buy":
             # 숏 포지션 보유중인 경우 숏포지션 정리후 롱포지션 진입
             if float(positionAmt) < 0.0:
+                # 매도/숏 포지션 정리
                 binance.create_order(
-                    symbol=symbol2,
+                    symbol=symbol,
                     type="MARKET",
                     side="buy",
                     amount=qty
                 )
+                # 매도/숏 포지션 진입
                 binance.create_order(
-                    symbol=symbol2,
+                    symbol=symbol,
                     type="MARKET",
                     side="buy",
                     amount=qty
                 )
                 # stop loss
                 binance.create_order(
-                    symbol=symbol2,
+                    symbol=symbol,
                     type="STOP_MARKET",
                     side="sell",
                     amount=qty,
-                    params={'stopPrice': current_price * 0.98}
+                    params={'stopPrice': current_price * longStopPrice}
                 )
         if orderType == "sell":
             # 롱 포지션 보유중인 경우 롱포지션 정리후 숏포지션 진입
             if float(positionAmt) > 0.0:
+                # 매수/롱 포지션 정리
                 binance.create_order(
-                    symbol=symbol2,
+                    symbol=symbol,
                     type="MARKET",
                     side="sell",
                     amount=qty
                 )
+                # 매수/롱 포지션 진입
                 binance.create_order(
-                    symbol=symbol2,
+                    symbol=symbol,
                     type="MARKET",
                     side="sell",
                     amount=qty
                 )
                 # stop loss
                 binance.create_order(
-                    symbol=symbol2,
+                    symbol=symbol,
                     type="STOP_MARKET",
                     side="buy",
                     amount=qty,
-                    params={'stopPrice': current_price*1.02}
+                    params={'stopPrice': current_price * shortStopPrice}
                 )
 
 if __name__ == '__main__':
